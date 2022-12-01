d5e1fef875e22bd7f5254acc79fc8510b2973353
mexpetr2 <clem8260@gmail.com>
2022-12-01 23:32:33 +0100

    v1

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
+        corrupti culpa fuga adipisci, tenetur atque, ipsum vel animi ratione
+        quibusdam, cupiditate explicabo voluptate tempore. Exercitationem nisi
+        fuga suscipit unde tempora molestias, doloribus placeat veritatis
+        dolorum ad. Ipsum hic possimus doloremque voluptas velit molestiae
+        voluptatum. Aperiam quibusdam magnam incidunt earum, vitae rem nobis.
+        Ducimus dicta nobis nulla pariatur recusandae, aperiam ad rerum
+        reiciendis totam unde molestias blanditiis reprehenderit soluta et
+        excepturi perspiciatis saepe molestiae. Quis ipsum quasi ut magnam
+        laborum sit rerum? Ab, illum quasi fugiat explicabo dolores nobis libero
+        eum laboriosam voluptates neque aut voluptas dolorum nam perferendis
+        fuga odit.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit hic quos
+        odit iste sed praesentium possimus, error soluta? Rerum nobis saepe
+        laudantium debitis illo minus magni ullam corrupti deserunt nisi!
+        Laboriosam ea ipsam possimus, id voluptatum culpa officiis aut
+        accusantium fugiat error aspernatur eum deserunt eaque magni minus odit
+        fuga animi recusandae ipsum delectus veniam aperiam? Natus voluptatibus,
+        ex odit illum qui in? Fugit aut laudantium eaque laborum, fugiat
+        voluptatem eum voluptatibus aspernatur officiis repudiandae totam quis
+        ad ex deserunt, praesentium dolorum. Similique quia ex voluptatum
+        molestiae beatae, animi fuga id molestias facere fugiat tenetur iste
+        sapiente illum ipsum modi impedit quasi eveniet dolores? Iure distinctio
+        suscipit, soluta et magnam a excepturi ducimus hic perferendis impedit
+        quasi totam ullam temporibus omnis vel nihil pariatur, rem natus
+        blanditiis praesentium libero nulla? Cum vel ad hic. Numquam doloremque
+        ipsa officiis cum quam possimus illo voluptas commodi, sunt vitae amet
+        sequi eligendi adipisci obcaecati beatae quis omnis nisi nemo facilis,
+        ipsam accusamus repellendus sint! Animi ipsum possimus a dolorum ut iste
+        accusantium nulla sequi, commodi vel magnam debitis esse! Vel amet quasi
+        vero officiis facere, quae nesciunt eligendi id omnis ipsum hic nam
+        deserunt aperiam voluptatibus rerum dicta distinctio quis, dolor quos
+        libero.
+      </p>
+    </div>
+  );
+};
+
+export default About;
